720a7765c935b2233a230d1bca7ad6510c13a79c
Brandon Dennis <toady00@gmail.com>
2014-08-08 16:48:29 -0400

    Update zookeeper to 3.4.6

--- lib/jruby-zk/version.rb
+++ lib/jruby-zk/version.rb
@@ -1,8 +1,8 @@
 module JRubyZk
-  VERSION               = '3.4.5'
+  VERSION               = '3.4.6'
   JAR_VERSIONS = {
     'jline'         => '0.9.94',
-    'log4j'         => '1.2.15',
+    'log4j'         => '1.2.16',
     'slf4j_api'     => '1.6.1',
     'slf4j_log4j12' => '1.6.1',
     'zookeeper'     => VERSION
